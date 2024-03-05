6feb0963ada29f61899926cc453ee2284d7b423a
akkayameva <meva@eywin.com.tr>
2024-03-05 21:22:03 +0300

    buildSrc created

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+
 }
 
 
@@ -67,4 +68,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(project(":core"))
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,5 +21,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "ecommerce"
 include(":app")
-
 include(":core")
+
