0aba47c060547aa1c0987b57c212bed7e0183938
akkayameva <meva@eywin.com.tr>
2024-03-05 20:32:17 +0300

    core module fixed

--- coretest/build.gradle.kts
+++ coretest/build.gradle.kts
@@ -10,9 +10,6 @@ android {
     defaultConfig {
         applicationId = "com.akkayameva.core"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -36,17 +33,7 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
+
 }
 
 dependencies {
