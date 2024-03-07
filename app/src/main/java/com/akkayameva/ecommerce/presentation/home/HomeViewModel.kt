de19d84942d5edd334a2916bdc355a60f1236c64
akkayameva <akkayameva@gmail.com>
2024-03-07 21:02:09 +0300

    HomeViewModel added

--- app/src/main/java/com/akkayameva/ecommerce/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/akkayameva/ecommerce/presentation/home/HomeViewModel.kt
@@ -1,4 +1,24 @@
 package com.akkayameva.ecommerce.presentation.home
 
-class HomeViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.akkayameva.core.domain.model.Product
+import com.akkayameva.core.domain.usecase.product.ProductUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+
+class HomeViewModel(
+    private val productUseCase: ProductUseCase,
+) : ViewModel() {
+
+    private val _listProductState: MutableStateFlow<List<Product>> = MutableStateFlow(listOf())
+    val listProductState: StateFlow<List<Product>> = _listProductState
+
+    fun getAllProducts() {
+        productUseCase.getAllProducts().onEach { listProduct ->
+            _listProductState.value = listProduct
+        }.launchIn(viewModelScope)
+    }
 }
